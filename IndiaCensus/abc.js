82a5d992b941d8d7cf72182dd64b5ac6c2062531
Sandy Singh <sonal.kumarsingh@hotmail.com>
2016-07-26 19:19:30 +0530

    done

--- IndiaCensus/abc.js
+++ IndiaCensus/abc.js
@@ -1,15 +1,65 @@
-var dat = data.split(/\s*\r\n\s*/);
-var head=dat[0].split(",");
-var hl=head.length;
-var arra=[];
-var build =function(line){
-for(var j=0;j<hl;j++){
-//     this[head[j]]=line[j];
-//   }
-//     }
-//  for(var i=1,l=dat.length;i<l-1;i++)
-//  {
-//    var line= dat[i].split(",");
-//      arra[i]=new build(line);
-//      line=null;
-//  }
+var fs = require('fs');
+var f = fs.readFileSync("India2011.csv","utf8");
+var lines = f.split("\n");
+var last = lines.length-1;//number of records
+var heading=lines[0].split(",");
+var categoryNames = ["Educational level - Literate without educational level - Persons",
+                      "Educational level - Below Primary - Persons",
+                      "Educational level - Primary - Persons",
+                      "Educational level - Middle - Persons",
+                      "Educational level - Matric/Secondary - Persons",
+                      "Educational level - Higher secondary/Intermediate/Pre-University/Senior secondary - Persons",
+                      "Educational level - Non-technical diploma or certificate not equal to degree - Persons",
+                      "Educational level - Technical diploma or certificate not equal to degree - Persons",
+                      "Educational level - Graduate & above - Persons",
+                      "Educational level - Unclassified - Persons"
+                    ];
+var categoryPositions = [];//this stores the value of the category names in the heading
+var processedCategories = [];
+
+var agegroup=heading.indexOf("Age-group"),
+    totrururb=heading.indexOf("Total/ Rural/ Urban");
+
+function Category(cat,val){
+    this.Category = cat;
+    this.Total = val;
+}
+
+for (counter=0; counter < categoryNames.length; counter++)
+{
+  categoryPositions[counter]=heading.indexOf(categoryNames[counter]);
+}
+
+for(var j=1; j<=last; j++)
+{
+    row=lines[j].split(",");
+    if((row[totrururb]==="Total")&&(row[agegroup]==="All ages"))
+    {
+      for(counter=0;counter<categoryNames.length;counter++){
+        var categoryName = categoryNames[counter];
+        var categoryPosition = categoryPositions[counter];
+        var categoryTotal = row[categoryPosition];
+
+        // if() {
+
+        // }
+        //search for category object in processedCategories, if found then add "categoryTotal" value
+        //in the "Total" property of that object
+
+        //else the below code will be executed
+        // else{
+        // var objCategory = new Category(categoryName,categoryTotal);
+        // processedCategories.push(objCategory);
+      // }
+      }
+    }
+}
+
+
+
+// var part2json=JSON.stringify(processedCategories);
+// console.log(part2json);
+
+// fs.writeFile('part2.json', part2json,'utf8', function (err) {
+// if (err) throw err;
+// });
--- IndiaCensus/node.js
+++ IndiaCensus/node.js
@@ -1,36 +1,78 @@
 var fs = require('fs');
 var f = fs.readFileSync("India2011.csv","utf8");
- var line = f.split("\n");
-   var ik = line.length-1;
+var line = f.split("\n");
+ var ik = line.length-1;
 
-  var hd=line[0].split(",");
- var a1=hd[5];
- var a2="Total";
- var cn=function(){
-      this[a1],
-      this[a2]
-  }
-var m=7;
- var b=[];
- for (var i = 0; i < 13; i++)
+var hd=line[0].split(",");
+var z=hd.indexOf("Age-group"),
+  z1=hd.indexOf("Total/ Rural/ Urban"),
+  z2=hd.indexOf("Literate - Persons");
+
+var a1=hd[z];
+var a2="Total";
+var cn=function(){
+    this[a1],
+    this[a2]
+}
+var m=6;
+var b=[];
+
+for (var i = 0; i < 27; i++)
+{
+  //from to 7 to 19
+  if(m>6 && m<20)
   {
-    var total=0;
-    for (var j = 1; j < ik; j++)
+  var total=0;
+  for (var j = 1; j < ik; j++)
+  {
+   n=line[j].split(",");
+   if((n[z]==m)&&(n[z1]==="Total"))
     {
-     n=line[j].split(",");
-     if((n[5]==m)&&(n[4]==="Total"))
-      {
-             total=total+parseInt(n[12]);
-             k=n[5];
-      }
+           total=total+parseInt(n[z2]);
+           k=n[z];
     }
-   b[i]=new cn();
-   b[i][a1]=k;
-   b[i][a2]=total;
+  }
+}
+//6 && 80
+else
+{
+  var total=0;
+  for (var j = 1; j < ik; j++)
+  {
+   n=line[j].split(",");
+   {
+     if(m==80)
+     {
+     hs=n[z].split("+");
+     hs1=hs[0];
+    }
+   else
+      {
+        hs=n[z].split("-");
+          hs1=hs[1];
+        }
+   }
+   if((hs1==m)&&(n[z1]==="Total"))
+    {
+           total=total+parseInt(n[z2]);
+           k=n[z];
+    }
+  }
+}
+
+
+ if(m>19&&m<80)
+ m+=5;
+ else
    m++;
  }
 
-console.log(b);
+ b[i]=new cn();
+ b[i][a1]=k;
+ b[i][a2]=total;
+
 var b1=JSON.stringify(b);
-//console.log(b1);
-fs.writeFile('out.json', b1,'utf8');
+/*console.log(b1);*/
+fs.writeFile('out.json', b1,'utf8', function (err) {
+if (err) throw err;
+});
